e6a1b2e1ef28f7445debd50857ecec345b026087
gguedes <gguedes@student.42.rio>
2022-07-21 11:25:30 -0300

    norm check quotes working

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:52:52 by gguedes           #+#    #+#             */
-/*   Updated: 2022/07/21 11:07:09 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/07/21 11:22:29 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,12 @@ static int	count_subs(char const *s, char c)
 	{
 		if (*s == 34)
 		{
-			s++;
-			while (*s != 34 && *s)
-				s++;
+			s = ft_strchr(s + 1, 34);
 			i++;
 		}
 		else if (*s == 39)
 		{
-			s++;
-			while (*s != 39 && *s)
-				s++;
+			s = ft_strchr(s + 1, 39);
 			i++;
 		}
 		else if (*s != c && (*(s + 1) == c || *(s + 1) == '\0')
