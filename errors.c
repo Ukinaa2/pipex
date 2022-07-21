972cc66bfa8d709a8f316b46d20e109fde7f1f85
gguedes <gguedes@student.42.rio>
2022-07-21 10:13:47 -0300

    a

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: gguedes <gguedes@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 13:19:42 by gguedes           #+#    #+#             */
-/*   Updated: 2022/07/19 13:17:34 by gguedes          ###   ########.fr       */
+/*   Updated: 2022/07/20 10:19:49 by gguedes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	error_case(int error_value)
 {
 	if (error_value == 1)
-		perror("Invalid number of argvs");
+		perror("Error value = 1: Invalid number of argvs");
 	else if (error_value == 2)
-		perror("Failed to initialize pipe");
+		perror("Error value = 2: Failed to initialize pipe");
 	else if (error_value == 3)
-		perror("Failed to fork");
+		perror("Error value = 3: Failed to fork");
 	else if (error_value == 4)
-		perror("Invalid fd");
+		perror("Error value = 4: Invalid fd");
 	else if (error_value == 5)
-		perror("Error executing command");
+		perror("Error value = 5: Error executing command");
 	return (error_value);
 }
